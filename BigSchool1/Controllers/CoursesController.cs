d3600ef6151b477d9b59005346cdc2d38df4eb19
Axeon <Axeon@DESKTOP-FVQBMPF>
2023-03-05 16:42:41 +0700

    Add project files.

---
+++ BigSchool1/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool1.Models;
+using BigSchool1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext= new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
