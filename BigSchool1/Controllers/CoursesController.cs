0957be861b2173fb7641ca031a07fb9fbf80bbde
Axeon <khoabmt12354@gmail.com>
2023-03-05 18:19:34 +0700

    fix

--- BigSchool1/Controllers/CoursesController.cs
+++ BigSchool1/Controllers/CoursesController.cs
@@ -17,6 +17,7 @@ namespace BigSchool1.Controllers
             _dbContext= new ApplicationDbContext();
         }
         [Authorize] 
+
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
